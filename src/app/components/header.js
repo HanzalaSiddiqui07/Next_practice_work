f025df05821e74b9f61ec057fefa12ffcb8d484d
Muhammad Hanzala Siddiqui <hanzalasiddiqui75@gmail.com>
2024-06-12 10:08:05 +0500

    Change in header title

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className="text-center text-5xl font-bold py-10 bg-slate-900 text-white">
-            <Link href="/">To-do List App</Link>
+            <Link href="/">Poduct List App</Link>
         </div>
     )
 }
